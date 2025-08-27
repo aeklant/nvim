0f2f804385daf30aa6fb6d4f4c8a006931601ac4
Abraham Escalante <aeklant@gmail.com>
2025-08-27 09:09:15 -0400

    ENH: add virtual text plugin for debugger

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,32 +1,40 @@
 return {
-  {
-    "mfussenegger/nvim-dap",
-    dependencies = {
-      {
-        "rcarriga/nvim-dap-ui",
-        dependencies = { "nvim-neotest/nvim-nio" },
-      },
-    },
+	{
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			{
+				"rcarriga/nvim-dap-ui",
+				dependencies = { "nvim-neotest/nvim-nio" },
+			},
+		},
+		config = function()
+			local dap = require("dap")
+			local dapui = require("dapui")
+			dapui.setup()
+
+			dap.listeners.before.attach.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.launch.dapui_config = function()
+				dapui.open()
+			end
+			dap.listeners.before.event_terminated.dapui_config = function()
+				dapui.close()
+			end
+			dap.listeners.before.event_exited.dapui_config = function()
+				dapui.close()
+			end
+
+			vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
+			vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+		end,
+	},
+	{
+    -- for display of variable values next to them in the code
+    -- TODO: customize setup
+		"theHamsta/nvim-dap-virtual-text",
     config = function()
-      local dap = require("dap")
-      local dapui = require("dapui")
-      dapui.setup()
-
-      dap.listeners.before.attach.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated.dapui_config = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited.dapui_config = function()
-        dapui.close()
-      end
-
-      vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
-      vim.keymap.set("n", "<Leader>dc", dap.continue, {})
-    end,
-  },
+      require("nvim-dap-virtual-text").setup()
+    end
+	},
 }
