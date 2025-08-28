6e52ca67f5ef1a855041c5448b6851e6be7c959e
Abraham Escalante <aeklant@gmail.com>
2025-08-28 13:23:20 -0400

    STY: set everforest as our colorscheme

--- lua/plugins/themes/everforest-nvim.lua
+++ lua/plugins/themes/everforest-nvim.lua
@@ -1,8 +1,8 @@
 return {
-	"neanias/everforest-nvim",
-	name = "everforest",
-	priority = 1000,
-	config = function()
-		vim.cmd("colorscheme everforest")
-	end,
+  "neanias/everforest-nvim",
+  name = "everforest",
+  priority = 1000,
+  config = function()
+    vim.cmd("colorscheme everforest")
+  end,
 }
