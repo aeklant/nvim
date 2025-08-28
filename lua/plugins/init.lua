5db11e174f3f0469936c99edc29661665beb7848
Abraham Escalante <aeklant@gmail.com>
2025-08-27 21:57:27 -0400

    DEV: add imports of plugins nested in subfolders (rust, themes)

---
+++ lua/plugins/init.lua
@@ -0,0 +1,10 @@
+return {
+  { -- Rust
+    { import = "plugins.rust.rustaceanvim" },
+    { import = "plugins.rust.crates-nvim" },
+  },
+	{ -- themes
+    { import = "plugins.themes.everforest-nvim" },
+    { import = "plugins.themes.nightfox-theme" },
+	},
+}
