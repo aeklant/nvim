92bc36adc2eb728637efd66edf4cd4527ed27bd9
Abraham Escalante <aeklant@gmail.com>
2026-02-09 09:43:33 -0500

    DEP: remove spelling auto-completion; it was more annoying than useful

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,7 +7,6 @@ return {
         null_ls.builtins.formatting.stylua, --lua
         null_ls.builtins.formatting.black, --python
         null_ls.builtins.formatting.isort, --python
-        null_ls.builtins.completion.spell,
       },
     })
 
