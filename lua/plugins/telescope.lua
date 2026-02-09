987230aad033c0d66749a4ef383c678239c3a91c
Abraham Escalante <aeklant@gmail.com>
2026-02-09 08:58:09 -0500

    ENH: change find_files keymap to <leader>ff; tag telescope fix

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,13 +1,13 @@
 return {
   {
-    "nvim-telescope/telescope.nvim", tag = "0.2.1",
+    "nvim-telescope/telescope.nvim", tag = "v0.2.1",
     dependencies = {
-	"nvim-lua/plenary.nvim",
-	{ "nvim-telescope/telescope-fzf-native.nvim", build="make" }
+      "nvim-lua/plenary.nvim",
+      { "nvim-telescope/telescope-fzf-native.nvim", build="make" }
     },
     config = function()
       local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
     end
   },
