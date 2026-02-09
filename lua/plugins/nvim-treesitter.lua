6ad9162cad349331259e727e7a8944f1da99000c
Abraham Escalante <aeklant@gmail.com>
2026-02-09 09:00:19 -0500

    BUG: fix - disable highlighting in preview mode since treesitter is broken; fix typo in a setup name

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,11 +1,21 @@
-return { 
-  "nvim-treesitter/nvim-treesitter", 
+return {
+  "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
+    -- disable treesitter in preview mode due to a bug (when highlighting)
+    require("telescope").setup({
+      defaults = {
+        preview = {
+          treesitter = false,
+        },
+      },
+    })
+
     local config = require("nvim-treesitter.config")
+
     config.setup({
       auto_install = true,
-      hightlight = { enable = true },
+      highlight = { enable = true },
       indent = { enable = true },
     })
   end
