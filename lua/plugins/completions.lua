04b038fad9d1fdcd1add77ef4b5d3a8ba9404730
Abraham Escalante <aeklant@gmail.com>
2026-02-15 12:54:54 -0500

    ENH: use Tab as auto-complete key (instead of Enter key)
    Add some logic so the behavior corresponds the situation

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -13,6 +13,7 @@ return {
     "hrsh7th/nvim-cmp",
     config = function()
       local cmp = require("cmp")
+      local luasnip = require("luasnip")
       require("luasnip.loaders.from_vscode").lazy_load()
 
       cmp.setup({
@@ -30,7 +31,30 @@ return {
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+
+          -- TAB
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.confirm({ select = true })
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          -- SHIFT + TAB
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
+
         sources = cmp.config.sources({
           { name = "nvim_lsp" },
           { name = "luasnip" },
