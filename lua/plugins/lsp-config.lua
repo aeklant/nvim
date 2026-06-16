cf78fa8897cae57f32c8d3ae3209b5b7ab8f801a
Abraham Escalante <aeklant@gmail.com>
2026-06-15 23:32:34 -0400

    DEV: add zig language server as LSP

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,6 +18,7 @@ return {
 		'neovim/nvim-lspconfig',
 		config = function()
 			--local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      vim.lsp.enable('zls')
 
       vim.lsp.config('rust_analyzer', {
         -- Server-specific settings. See `:help lsp-quickstart`
